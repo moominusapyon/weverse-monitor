6bfc658d4a07149cb14bc805d65de91d8ec80eaa
moominusapyon <moominusapyon@gmail.com>
2026-04-05 16:37:28 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,12 +1,46 @@
 import requests
+import os
 
-URL = "https://share.weverseshop.io/static/shares/sale/43782"
+URL = "https://shop.weverse.io/en/shop/KRW/artists/2/sales/56488"
 WEBHOOK = "https://discord.com/api/webhooks/1490372167983173885/hKmgaGr1Xeuh268ncP7FEyX5c91y6eMOzv5H7FiXa9TyffYSvOnnFE8Gu49BgMb35Jyx"
 
-r = requests.get(URL, allow_redirects=True)
-html = r.text.lower()
+STATE_FILE = "status.txt"
 
-if "sold out" not in html and "품절" not in html:
+
+def get_status():
+    r = requests.get(URL, allow_redirects=True)
+    html = r.text.lower()
+
+    if "sold out" in html or "품절" in html:
+        return "soldout"
+    else:
+        return "instock"
+
+
+def read_last_status():
+    if not os.path.exists(STATE_FILE):
+        return None
+    with open(STATE_FILE, "r") as f:
+        return f.read().strip()
+
+
+def save_status(status):
+    with open(STATE_FILE, "w") as f:
+        f.write(status)
+
+
+def send_discord():
     requests.post(WEBHOOK, json={
-        "content": f"🚨 Weverse可能補貨！\n{URL}"
+        "content": f"🚨 Weverse補貨！\n{URL}"
     })
+
+
+current = get_status()
+last = read_last_status()
+
+print("last:", last, "| current:", current)
+
+if last == "soldout" and current == "instock":
+    send_discord()
+
+save_status(current)
